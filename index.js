7c444bcb6b59cba502e16358ae6fb643628b6f5f
gongsi <1141264013@qq.com>
2021-06-10 19:18:56 +0800

    提交保存

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const multer = require('multer')
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads/')
+    cb(null, './static/uploads/')
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname)
@@ -56,6 +56,7 @@ app.post('/api/login', urlencodedParser, (req, res) => {
         <script>
           alert('用户名密码匹配成功')
           window.location = '/'
+          window.localStorage.setItem('username', ${req.body.username})
         </script>
       `)
     } else {
@@ -107,7 +108,7 @@ app.post('/api/profile', upload.single('avatar'), function (req, res, next) {
     database: 'my_test'
   })
   connection.connect()
-  connection.query(`INSERT INTO sys_user (avator) VALUES ('${req.file.filename}') where username = 201312588103`, function (error, results, fields) {
+  connection.query(`update sys_user set avator = '${req.file.filename}' where username = '201312588103'`, function (error, results, fields) {
     if (error) throw error
     res.send(`
       <script>
@@ -115,6 +116,70 @@ app.post('/api/profile', upload.single('avatar'), function (req, res, next) {
         window.location = '/upload'
       </script>
     `)
+    connection.end()
+  })
+})
+
+app.get('/check', urlencodedParser, (req, res) => {
+  const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'my_test'
+  })
+  connection.connect()
+  connection.query(`select * from sys_user where username = ?`, [req.query.username], function (error, results, fields) {
+    if (error) throw error
+    if (results.length > 0) {
+      res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>作业提交系统-用户登录</title>
+          <link rel="stylesheet" href="../css/index.css">
+        </head>
+        <body>
+          <header>
+            <h2>
+              <a href="/">
+                <img src="../images/tupian1.png" alt="">
+              </a>
+            </h2>
+          </header>
+          <nav>
+            <div>
+              <ul>
+                <li><a href="/upload">上传作业</a></li>
+                <li><a href="/check?username=${req.query.username}">查看作业</a></li>
+                <li><a href="javascript:;">作业公告</a></li>
+                <li><a href="javascript:;">关于系统</a></li>
+                <li><a href="/">登录</a></li>
+                <li><a href="/changepassword">修改密码</a></li>
+                <li><a href="/register">注册</a></li>
+              </ul>
+            </div>
+          </nav>
+          <div>
+            <div class="form1">
+              <a href="/uploads/${results[0].avator}" download="${results[0].avator}">${results[0].avator}</a>
+            </div>
+          </div>
+        
+          <script src="../js/index.js"></script>
+        </body>
+        </html>
+      `)
+    } else {
+      res.send(`
+        <script>
+          alert('查询失败')
+          window.location.href = '/'
+        </script>
+      `)
+    }
     connection.end()
   })
 })
